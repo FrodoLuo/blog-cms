ecbc17bcb2632a2da0cfa630ff484275d7d132c8
FrodoLuo <frodoluo@outlook.com>
2020-07-24 00:21:11 +0800

    init: set up framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './layouts/main/main.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(mod => mod.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    children: [
+      {
+        path: 'article',
+        loadChildren: () => import('./modules/article/article.module').then(mod => mod.ArticleModule)
+      },
+      {
+        path: 'media',
+        loadChildren: () => import('./modules/media/media.module').then(mod => mod.MediaModule)
+      }
+    ],
+    component: MainComponent,
+    canActivate: [AuthGuard]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
