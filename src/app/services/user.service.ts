61fbf523c56a951ea66a7adef64f20ef537527e9
FrodoLuo <frodoluo@outlook.com>
2020-07-31 00:14:14 +0800

    feat: login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { AES, MD5, mode, pad } from 'crypto-js';
+import { AES, MD5, mode, pad, enc } from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -9,27 +9,18 @@ export class UserService {
 
   constructor(
     private http: HttpClient
-  ) {}
-  
+  ) { }
+
   public requestLogIn(email, password) {
-    const randomKey = Date.now().toFixed(0);
-    const cipheredPassword = AES.encrypt(MD5(password).toString(), randomKey, {
-      mode: mode.ECB,
-      padding: pad.ZeroPadding
-    });
-    console.log(cipheredPassword.ciphertext.toString());
+    const randomKey = this.padTo16(Date.now().toString());
+    const cipheredPassword = this.cipherPassword(password, randomKey)
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', cipheredPassword);
+    formData.append('key', randomKey);
     this.http.post(
       '/api/users',
-      {
-        email,
-        password: cipheredPassword.toString(),
-        key: randomKey
-      },
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }
+      formData,
     ).subscribe(
       res => {
         console.log(res);
@@ -38,5 +29,27 @@ export class UserService {
         console.log(error);
       }
     );
+  }
+
+  private cipherPassword(origin: string, passphase: string): string {
+    const encodedOrigin = enc.Utf8.parse(MD5(origin).toString());
+    const encodedKey = enc.Utf8.parse(passphase)
+    const cipheredPassword = AES.encrypt(encodedOrigin, encodedKey, {
+      mode: mode.CBC,
+      iv: encodedKey,
+      padding: pad.ZeroPadding
+    });
+    return enc.Base64.stringify(cipheredPassword.ciphertext)
+  }
+  private padTo16(origin: string): string {
+    if (this.padTo16.length > 16) {
+      return origin.slice(0, 16);
+    } else {
+      let result = origin;
+      for (let i = 0; i < 16 - origin.length; i++) {
+        result += '0'
+      }
+      return result;
+    }
   }
 }
